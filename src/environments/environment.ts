7cab6ddee9100c3d69412e5c33fe0a0f8167f2e8
BuchananSEO <68229983+BuchananSEO@users.noreply.github.com>
2022-06-11 21:06:19 -0700

    realm

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  APP_ID: '<your-app-id>',
-  GRAPHQL_URI: '<your-graphql-uri>',
-  API_KEY: '<your-api-key>',
+  APP_ID: 'uber-apollo-vgzrt',
+  GRAPHQL_URI: 'https://us-east-1.aws.realm.mongodb.com/api/client/v2.0/app/uber-apollo-vgzrt/graphql',
+  API_KEY: 'vN1W4CPQqVdKVo6q8Dd2vScJiPq7MYvidhJb28TZ5v4Lw4qzqXOTlUddMU0uQNuQ',
 };
